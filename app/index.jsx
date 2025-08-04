dfd80ddf3a8c13462ef6a9dec9ee0d32dc2469b4
g-russo <russpeter.garcia.cics@ust.edu.ph>
2025-08-04 22:28:45 +0800

    Innitial commit

---
+++ app/index.jsx
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from "react-native";
+const Home = () => {
+  return (
+    <View>
+      <Text>Welcome to the Home Screen!</Text>
+    </View>
+  );
+}
+export default Home
+
+const styles = StyleSheet.create({});
